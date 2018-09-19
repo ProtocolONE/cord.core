94b406822fe84cc2b509c31affbffd5f65c1a41e
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2018-09-19 17:09:26 +0300

    Fix conan build

--- build.conan.cmd
+++ build.conan.cmd
@@ -2,7 +2,7 @@ set PackageName=Core/1.0.0@common/stable
 
 @call install.deps.cmd
 
-conan create . %PackageName% -pr msvcprofile
-conan create . %PackageName% -pr msvcprofiled
+conan create . %PackageName% -pr conan/msvcprofile
+conan create . %PackageName% -pr conan/msvcprofiled
 
 conan upload %PackageName% --all -r=p1
